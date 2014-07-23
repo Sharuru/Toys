f72e3c2f2df8e420ddad0ab1f35a6e790f8728a1
Mave <i@0w0.re>
2014-07-23 15:08:47 +0800

    Init Web.py Play
    New: Just a toy.

---
+++ Web.py Play/Web.py Play/code.py
@@ -0,0 +1,19 @@
+﻿import web
+import string
+
+render = web.template.render('templates/')
+
+urls = (
+  '/(.*)', 'sites'
+)
+
+class sites:
+    def GET(self, dest):
+        if dest:
+            return render.template(dest)
+        else:
+            return "This is HomePage"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
