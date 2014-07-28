21c08fd9f79a6e64953210962b8bfa342e4aedee
Mave <i@0w0.re>
2014-07-28 14:48:22 +0800

    newPost Module Created.
    New: Init Moudle

--- Web.py Play/Web.py Play/code.py
+++ Web.py Play/Web.py Play/code.py
@@ -1,18 +1,33 @@
 ﻿import web
-import string
+from web import form
 
 render = web.template.render('templates/')
 
+#url跳转
 urls = (
-  '/(.*)', 'sites'
+        '/', 'Index',
+        '/viewPost', 'ViewPost',
+        '/newPost', 'NewPost',
+        '/deletePost', 'DeletePost'
 )
 
-class sites:
-    def GET(self, dest):
-        if dest:
-            return render.template(dest)
-        else:
-            return "This is HomePage"
+#form表单
+##新留言表单
+newPostForm = form.Form(
+                        form.Textbox('Your Name'),
+                        form.Textarea('Message'),
+                        form.Button('Post it!')
+)
+
+class Index:
+    def GET(self):
+        post = newPostForm()
+        return render.index()
+
+class NewPost:
+    def GET(self):
+        post = newPostForm()
+        return render.newPost(post)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
