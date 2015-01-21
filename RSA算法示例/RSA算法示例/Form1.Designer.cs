e13df5206fb631aeff813b5099172fd4053f4d2f
Sharuru <mave@foxmail.com>
2015-01-21 23:18:51 +0800

    Basic design finished

--- RSA算法示例/RSA算法示例/Form1.Designer.cs
+++ RSA算法示例/RSA算法示例/Form1.Designer.cs
@@ -39,8 +39,22 @@
             this.textBoxGuide = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownP = new System.Windows.Forms.NumericUpDown();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxC1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxP = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxC2 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownP)).BeginInit();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -55,7 +69,7 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(286, 235);
+            this.groupBox1.Size = new System.Drawing.Size(306, 235);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "产生公钥和私钥";
@@ -112,48 +126,60 @@
             // 
             this.buttonCreateKey.Location = new System.Drawing.Point(91, 36);
             this.buttonCreateKey.Name = "buttonCreateKey";
-            this.buttonCreateKey.Size = new System.Drawing.Size(189, 32);
+            this.buttonCreateKey.Size = new System.Drawing.Size(209, 32);
             this.buttonCreateKey.TabIndex = 3;
             this.buttonCreateKey.Text = "产生密钥";
             this.buttonCreateKey.UseVisualStyleBackColor = true;
+            this.buttonCreateKey.Click += new System.EventHandler(this.buttonCreateKey_Click);
             // 
             // textBoxPQ
             // 
             this.textBoxPQ.Location = new System.Drawing.Point(91, 74);
             this.textBoxPQ.Name = "textBoxPQ";
-            this.textBoxPQ.Size = new System.Drawing.Size(189, 21);
+            this.textBoxPQ.ReadOnly = true;
+            this.textBoxPQ.Size = new System.Drawing.Size(209, 21);
             this.textBoxPQ.TabIndex = 4;
+            this.textBoxPQ.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBoxEN
             // 
             this.textBoxEN.Location = new System.Drawing.Point(91, 101);
             this.textBoxEN.Name = "textBoxEN";
-            this.textBoxEN.Size = new System.Drawing.Size(189, 21);
+            this.textBoxEN.ReadOnly = true;
+            this.textBoxEN.Size = new System.Drawing.Size(209, 21);
             this.textBoxEN.TabIndex = 5;
+            this.textBoxEN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBoxDN
             // 
             this.textBoxDN.Location = new System.Drawing.Point(91, 128);
             this.textBoxDN.Name = "textBoxDN";
-            this.textBoxDN.Size = new System.Drawing.Size(189, 21);
+            this.textBoxDN.ReadOnly = true;
+            this.textBoxDN.Size = new System.Drawing.Size(209, 21);
             this.textBoxDN.TabIndex = 6;
+            this.textBoxDN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBoxGuide
             // 
             this.textBoxGuide.Location = new System.Drawing.Point(7, 156);
             this.textBoxGuide.Multiline = true;
             this.textBoxGuide.Name = "textBoxGuide";
-            this.textBoxGuide.Size = new System.Drawing.Size(273, 72);
+            this.textBoxGuide.Size = new System.Drawing.Size(293, 72);
             this.textBoxGuide.TabIndex = 7;
             this.textBoxGuide.Text = "1.选择2个保密的大素数p和q;\r\n2.计算n=p·q和欧拉函数值φ(n)=(p-1)(q-1); \r\n3.选一整数e,且满足1<e<φ(n)和gcd(φ(n)," +
     "e)=1;\r\n4.计算d，且满足d·e=1 mod φ(n);\r\n5.公钥为{e,n}, 私钥为{d,n}";
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBoxC1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.buttonEncrypt);
+            this.groupBox2.Controls.Add(this.numericUpDownP);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Location = new System.Drawing.Point(12, 254);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(286, 72);
+            this.groupBox2.Size = new System.Drawing.Size(306, 76);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "加密";
@@ -167,19 +193,150 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "明文P=";
             // 
+            // numericUpDownP
+            // 
+            this.numericUpDownP.Location = new System.Drawing.Point(49, 19);
+            this.numericUpDownP.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownP.Name = "numericUpDownP";
+            this.numericUpDownP.Size = new System.Drawing.Size(62, 21);
+            this.numericUpDownP.TabIndex = 1;
+            this.numericUpDownP.Value = new decimal(new int[] {
+            19,
+            0,
+            0,
+            0});
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(118, 19);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(58, 23);
+            this.buttonEncrypt.TabIndex = 2;
+            this.buttonEncrypt.Text = "加密";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "密文C=";
+            // 
+            // textBoxC1
+            // 
+            this.textBoxC1.Location = new System.Drawing.Point(230, 19);
+            this.textBoxC1.Name = "textBoxC1";
+            this.textBoxC1.ReadOnly = true;
+            this.textBoxC1.Size = new System.Drawing.Size(70, 21);
+            this.textBoxC1.TabIndex = 4;
+            this.textBoxC1.Text = "?";
+            this.textBoxC1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "使用公钥{e, n}\r\nC = P^e mod n\r\n";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBoxC2);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.textBoxP);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.buttonDecrypt);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Location = new System.Drawing.Point(12, 336);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(306, 76);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "解密";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(106, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 24);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "使用私钥{d, n}\r\nP = C^d mod n\r\n";
+            // 
+            // textBoxP
+            // 
+            this.textBoxP.Location = new System.Drawing.Point(230, 19);
+            this.textBoxP.Name = "textBoxP";
+            this.textBoxP.ReadOnly = true;
+            this.textBoxP.Size = new System.Drawing.Size(70, 21);
+            this.textBoxP.TabIndex = 4;
+            this.textBoxP.Text = "?";
+            this.textBoxP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(182, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "明文P=";
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Location = new System.Drawing.Point(118, 19);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(58, 23);
+            this.buttonDecrypt.TabIndex = 2;
+            this.buttonDecrypt.Text = "解密";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "密文C=";
+            // 
+            // textBoxC2
+            // 
+            this.textBoxC2.Location = new System.Drawing.Point(49, 19);
+            this.textBoxC2.Name = "textBoxC2";
+            this.textBoxC2.Size = new System.Drawing.Size(63, 21);
+            this.textBoxC2.TabIndex = 6;
+            this.textBoxC2.Text = "?";
+            this.textBoxC2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(310, 379);
+            this.ClientSize = new System.Drawing.Size(330, 419);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "RSA算法示例";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownP)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -197,6 +354,18 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxC1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.NumericUpDown numericUpDownP;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxP;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxC2;
     }
 }
 
--- RSA算法示例/RSA算法示例/Form1.cs
+++ RSA算法示例/RSA算法示例/Form1.cs
@@ -15,5 +15,127 @@ namespace RSA算法示例
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //程序Load时，自动选择2个素数，默认p=7，q=17
+            listBoxP.SelectedIndex = 1;
+            listBoxQ.SelectedIndex = 5;
+            //程序Load时，自动生成一个明文P，默认P=19
+            numericUpDownP.Value = 19;
+            //程序Load时，将密文C以及解密明文P置问号
+            textBoxC1.Text = "?";
+            textBoxC2.Text = "?";
+            textBoxP.Text = "?";
+        }
+
+        private void buttonCreateKey_Click(object sender, EventArgs e)
+        {
+            //按下“产生密钥”后开始计算
+            long n, p, q, ee, dd, phi;
+            p = Convert.ToInt64(listBoxP.SelectedItem);
+            q = Convert.ToInt64(listBoxQ.SelectedItem);
+            n = p*q;
+            phi = (p - 1)*(q - 1);
+            textBoxPQ.Text = "n = pq = " + n;
+            ee = CalcE(phi);
+            textBoxEN.Text = "公钥为{e,n} = { " + ee + ", " + n + "}";
+            dd = CalcD(phi, ee);
+            textBoxDN.Text = "私钥为{d,n} = { " + dd + ", " + n + "}";
+        }
+
+        private long CalcE(long phi)
+        {
+            //由于两个不相同的质数必定互素，为提高计算效率预先硬编码1~37*37的质数表
+            long[] prime =
+            {
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+                101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
+                197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307,
+                311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421,
+                431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547,
+                557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659,
+                661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+                809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929,
+                937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039,
+                1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151,
+                1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259,
+                1277, 1279, 1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367
+            };
+            //随机选择e
+            Random Re = new Random();
+            return 5;
+//             while(true)
+//             {
+//                 int i = Re.Next(218);
+//                 if (prime[i] < phi)
+//                 {
+//                     return prime[i];
+//                 }
+//             }
+        }
+
+        private long CalcD(long phi, long ee)
+        {
+            for (long d = 1;; d++)
+            {
+                if ((ee*d)%phi == 1)
+                {
+                    return d;
+                }
+            }
+        }
+
+        private long Encrypt(long ee, long n)
+        {
+            return (long) (Math.Pow(Convert.ToInt64(numericUpDownP.Value),ee))%n;
+        }
+
+        private long Decrypt(long dd, long n)
+        {
+            //return (long) (Math.Pow(Convert.ToInt64(textBoxC2.Text),dd));
+            long ans = 1;
+            long a = Convert.ToInt64(textBoxC2.Text);
+            long c = n;
+            a = a%c;
+            long b = dd;
+            while (b > 0)
+            {
+                if (b%2 == 1)
+                {
+                    ans = (ans*a)%c;
+                }
+                b = b / 2;
+                a = (a * a) % c;
+            }
+            return ans;
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            long n, p, q, ee, dd, phi, cc;
+            p = Convert.ToInt64(listBoxP.SelectedItem);
+            q = Convert.ToInt64(listBoxQ.SelectedItem);
+            n = p*q;
+            phi = (p - 1)*(q - 1);
+            ee = CalcE(phi);
+            dd = CalcD(phi, ee);
+            cc = Encrypt(5, n);
+            textBoxC1.Text = cc.ToString();
+            textBoxC2.Text = cc.ToString();
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            long n, p, q, ee, dd, phi, cc;
+            p = Convert.ToInt64(listBoxP.SelectedItem);
+            q = Convert.ToInt64(listBoxQ.SelectedItem);
+            n = p * q;
+            phi = (p - 1) * (q - 1);
+            ee = CalcE(phi);
+            dd = CalcD(phi, ee);
+            p = Decrypt(dd, n);
+            textBoxP.Text = p.ToString();
+        }
     }
 }
