2fff1a415134ce795c5b91e6a5d0604cf8f7cc88
Sharuru <mave@foxmail.com>
2015-05-09 22:40:18 +0800

    Add publish info

--- EasyCertManager/EasyCertManager/Properties/AssemblyInfo.cs
+++ EasyCertManager/EasyCertManager/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("EasyCertManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An easy cert manager")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sharuru Personal")]
 [assembly: AssemblyProduct("EasyCertManager")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
