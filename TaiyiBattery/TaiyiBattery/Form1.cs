86d81534a4857e5cf08326defee2768c6fdf2015
Sharuru <mave@foxmail.com>
2015-07-02 14:33:51 +0800

    Cleanup code

--- TaiyiBattery/TaiyiBattery/Form1.cs
+++ TaiyiBattery/TaiyiBattery/Form1.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace TaiyiBattery
@@ -19,33 +13,33 @@ namespace TaiyiBattery
         private void FormMain_Load(object sender, EventArgs e)
         {
             timerSetTime.Interval = 1000;
-            timerSetTime.Tick += new System.EventHandler(this.timerSetTime_Tick);
+            timerSetTime.Tick += timerSetTime_Tick;
             timerSetTime.Start();
-            calcTaiyi();
         }
 
         private void timerSetTime_Tick(object sender, EventArgs e)
         {
             labelTime.Text = DateTime.Now.ToLongTimeString();
+            CalcTaiyi();
         }
 
-        private void calcTaiyi()
+        private void CalcTaiyi()
         {
             TimeSpan ts = dateTimePickerEnd.Value - dateTimePickerStart.Value;
             TimeSpan td = dateTimePickerEnd.Value - DateTime.Now;
             double left = td.TotalSeconds/ts.TotalSeconds;
             progressBarBattery.Value = Convert.ToInt32(left*100);
-            labelBattery.Text = progressBarBattery.Value.ToString() + "%";
+            labelBattery.Text = progressBarBattery.Value + @"%";
         }
 
         private void dateTimePickerStart_ValueChanged(object sender, EventArgs e)
         {
-            calcTaiyi();
+            CalcTaiyi();
         }
 
         private void dateTimePickerEnd_ValueChanged(object sender, EventArgs e)
         {
-            calcTaiyi();
+            CalcTaiyi();
         }
     }
 }
