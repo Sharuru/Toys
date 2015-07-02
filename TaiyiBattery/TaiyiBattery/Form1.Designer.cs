32d1372ad5990599c9ab8dd0fee3397dd4e86890
Sharuru <mave@foxmail.com>
2015-07-02 14:31:45 +0800

    Taiyi usable

--- TaiyiBattery/TaiyiBattery/Form1.Designer.cs
+++ TaiyiBattery/TaiyiBattery/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TaiyiBattery
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -29,11 +29,138 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.labelGuide1 = new System.Windows.Forms.Label();
+            this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
+            this.labelGuide2 = new System.Windows.Forms.Label();
+            this.labelGuide3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBarBattery = new System.Windows.Forms.ProgressBar();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.timerSetTime = new System.Windows.Forms.Timer(this.components);
+            this.labelBattery = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelGuide1
+            // 
+            this.labelGuide1.AutoSize = true;
+            this.labelGuide1.Location = new System.Drawing.Point(12, 54);
+            this.labelGuide1.Name = "labelGuide1";
+            this.labelGuide1.Size = new System.Drawing.Size(155, 12);
+            this.labelGuide1.TabIndex = 0;
+            this.labelGuide1.Text = "今天太医的充电完成时间是:";
+            // 
+            // dateTimePickerStart
+            // 
+            this.dateTimePickerStart.CustomFormat = "HH:mm";
+            this.dateTimePickerStart.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerStart.Location = new System.Drawing.Point(173, 48);
+            this.dateTimePickerStart.Name = "dateTimePickerStart";
+            this.dateTimePickerStart.ShowUpDown = true;
+            this.dateTimePickerStart.Size = new System.Drawing.Size(138, 21);
+            this.dateTimePickerStart.TabIndex = 1;
+            this.dateTimePickerStart.Value = new System.DateTime(2015, 7, 2, 11, 30, 0, 0);
+            this.dateTimePickerStart.ValueChanged += new System.EventHandler(this.dateTimePickerStart_ValueChanged);
+            // 
+            // dateTimePickerEnd
+            // 
+            this.dateTimePickerEnd.CustomFormat = "HH:mm";
+            this.dateTimePickerEnd.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(185, 79);
+            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
+            this.dateTimePickerEnd.ShowUpDown = true;
+            this.dateTimePickerEnd.Size = new System.Drawing.Size(126, 21);
+            this.dateTimePickerEnd.TabIndex = 3;
+            this.dateTimePickerEnd.Value = new System.DateTime(2015, 7, 2, 22, 0, 0, 0);
+            this.dateTimePickerEnd.ValueChanged += new System.EventHandler(this.dateTimePickerEnd_ValueChanged);
+            // 
+            // labelGuide2
+            // 
+            this.labelGuide2.AutoSize = true;
+            this.labelGuide2.Location = new System.Drawing.Point(12, 85);
+            this.labelGuide2.Name = "labelGuide2";
+            this.labelGuide2.Size = new System.Drawing.Size(167, 12);
+            this.labelGuide2.TabIndex = 2;
+            this.labelGuide2.Text = "预计今天太医的没电的时间是:";
+            // 
+            // labelGuide3
+            // 
+            this.labelGuide3.AutoSize = true;
+            this.labelGuide3.Location = new System.Drawing.Point(12, 23);
+            this.labelGuide3.Name = "labelGuide3";
+            this.labelGuide3.Size = new System.Drawing.Size(71, 12);
+            this.labelGuide3.TabIndex = 4;
+            this.labelGuide3.Text = "现在时间是:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "太医目前的剩余电量为:";
+            // 
+            // progressBarBattery
+            // 
+            this.progressBarBattery.Location = new System.Drawing.Point(14, 148);
+            this.progressBarBattery.Name = "progressBarBattery";
+            this.progressBarBattery.Size = new System.Drawing.Size(297, 19);
+            this.progressBarBattery.Step = 1;
+            this.progressBarBattery.TabIndex = 7;
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(89, 23);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(53, 12);
+            this.labelTime.TabIndex = 8;
+            this.labelTime.Text = "00:00:00";
+            // 
+            // labelBattery
+            // 
+            this.labelBattery.AutoSize = true;
+            this.labelBattery.Location = new System.Drawing.Point(149, 117);
+            this.labelBattery.Name = "labelBattery";
+            this.labelBattery.Size = new System.Drawing.Size(29, 12);
+            this.labelBattery.TabIndex = 9;
+            this.labelBattery.Text = "100%";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(321, 185);
+            this.Controls.Add(this.labelBattery);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.progressBarBattery);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelGuide3);
+            this.Controls.Add(this.dateTimePickerEnd);
+            this.Controls.Add(this.labelGuide2);
+            this.Controls.Add(this.dateTimePickerStart);
+            this.Controls.Add(this.labelGuide1);
+            this.Name = "FormMain";
+            this.Text = "太医电量";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelGuide1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
+        private System.Windows.Forms.Label labelGuide2;
+        private System.Windows.Forms.Label labelGuide3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBarBattery;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Timer timerSetTime;
+        private System.Windows.Forms.Label labelBattery;
     }
 }
 
--- TaiyiBattery/TaiyiBattery/Form1.cs
+++ TaiyiBattery/TaiyiBattery/Form1.cs
@@ -9,11 +9,43 @@ using System.Windows.Forms;
 
 namespace TaiyiBattery
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            timerSetTime.Interval = 1000;
+            timerSetTime.Tick += new System.EventHandler(this.timerSetTime_Tick);
+            timerSetTime.Start();
+            calcTaiyi();
+        }
+
+        private void timerSetTime_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void calcTaiyi()
+        {
+            TimeSpan ts = dateTimePickerEnd.Value - dateTimePickerStart.Value;
+            TimeSpan td = dateTimePickerEnd.Value - DateTime.Now;
+            double left = td.TotalSeconds/ts.TotalSeconds;
+            progressBarBattery.Value = Convert.ToInt32(left*100);
+            labelBattery.Text = progressBarBattery.Value.ToString() + "%";
+        }
+
+        private void dateTimePickerStart_ValueChanged(object sender, EventArgs e)
+        {
+            calcTaiyi();
+        }
+
+        private void dateTimePickerEnd_ValueChanged(object sender, EventArgs e)
+        {
+            calcTaiyi();
         }
     }
 }
