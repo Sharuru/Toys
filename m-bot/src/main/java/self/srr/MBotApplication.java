63b36370f0810dbff573357bb5ca24906a79bd67
Sharuru <mave@foxmail.com>
2017-04-28 17:12:52 +0900

    m bot basic infra

---
+++ m-bot/src/main/java/self/srr/MBotApplication.java
@@ -0,0 +1,12 @@
+package self.srr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MBotApplication.class, args);
+	}
+}
