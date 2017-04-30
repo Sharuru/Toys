e1fbdcf3afa276b73f6f2c9393794997514f07fc
Sharuru <mave@foxmail.com>
2017-04-30 20:05:53 +0800

    update sample ddl

--- m-bot/ddl/fish_record.sql
+++ m-bot/ddl/fish_record.sql
@@ -9,7 +9,7 @@ Target Server Type    : SQLite
 Target Server Version : 31300
 File Encoding         : 65001
 
-Date: 2017-04-29 21:36:37
+Date: 2017-04-30 20:05:00
 */
 
 PRAGMA foreign_keys = OFF;
@@ -29,5 +29,7 @@ PRIMARY KEY ("id" ASC)
 -- ----------------------------
 -- Records of fish_record
 -- ----------------------------
-INSERT INTO "main"."fish_record" VALUES (1, 'thisIsTestUserName', 1493427600, 1493429700);
+INSERT INTO "main"."fish_record" VALUES (1, 'thisIsTestUserName', 1493429100, 1493429100);
+INSERT INTO "main"."fish_record" VALUES (2, 'thisIsTestUserName', 1493514000, 1493526300);
+INSERT INTO "main"."fish_record" VALUES (3, 'thisIsTestUserName1', 1493512980, 1493512980);
 PRAGMA foreign_keys = ON;
