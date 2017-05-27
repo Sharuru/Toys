89a4d98535dd8644fd7fd0037ec8acb6ad7123dd
Sharuru <mave@foxmail.com>
2017-05-27 21:52:50 +0800

    充值 web infra

---
+++ m-bot/src/main/java/self/srr/roll/RollPageController.java
@@ -0,0 +1,20 @@
+package self.srr.roll;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Sharuru on 2017/5/27 0027.
+ */
+@Controller
+@RequestMapping("/roll")
+@Slf4j
+public class RollPageController {
+
+    @RequestMapping(value="/switch", method = RequestMethod.GET)
+    public String index(){
+        return "roll/switch";
+    }
+}
