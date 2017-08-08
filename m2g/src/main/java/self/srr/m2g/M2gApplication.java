6a0b896787bb3e0fac98c51087e6406ba78ffa66
Sharuru <mave@foxmail.com>
2017-08-08 18:23:40 +0900

    m2g basic infra
    Export Microsoft Project MPP files to Gitlab issues.

---
+++ m2g/src/main/java/self/srr/m2g/M2gApplication.java
@@ -0,0 +1,12 @@
+package self.srr.m2g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2gApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2gApplication.class, args);
+	}
+}
---
+++ m2g/src/test/java/self/srr/m2g/M2gApplicationTests.java
@@ -0,0 +1,16 @@
+package self.srr.m2g;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class M2gApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
