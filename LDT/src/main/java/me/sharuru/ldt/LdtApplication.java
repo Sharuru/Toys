04161972ba4792041c0bf8fb7ec78c9795d6e33e
Sharuru <mave@foxmail.com>
2021-03-31 16:36:51 +0900

    LDT v0.1.0 bump up

--- LDT/src/main/java/me/sharuru/ldt/LdtApplication.java
+++ LDT/src/main/java/me/sharuru/ldt/LdtApplication.java
@@ -8,12 +8,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
 
+import java.io.File;
+import java.nio.file.Files;
 import java.util.List;
 
+/**
+ * Main entrypoint of the LDT
+ */
 @Slf4j
 @SpringBootApplication
-// TODO
 public class LdtApplication implements CommandLineRunner {
 
     /**
@@ -40,21 +45,29 @@ public class LdtApplication implements CommandLineRunner {
     @Value("${outputFilename:}")
     String outputFileName;
 
+    @Autowired
+    BuildProperties buildProperties;
+
+    private static final String DEFAULT_INPUT_FILENAME = "console-output.log";
+    private static final String DEFAULT_RESULT_FILENAME = "diagnostic-result.csv";
+
     public static void main(String[] args) {
         SpringApplication.run(LdtApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        log.info("Starting LDT...");
-        log.info("logPath: {}", inputPath);
-        if(inputPath.isEmpty()){
-            log.error("Log path was not determined.");
-            System.exit(-1);
+        log.info("Starting LDT v{}...", buildProperties.getVersion());
+        inputPath = inputPath.isEmpty() ? System.getProperty("user.dir") + File.separator + DEFAULT_INPUT_FILENAME : inputPath;
+        log.info("inputPath: {}", inputPath);
+        outputPath = outputPath.isEmpty() ? System.getProperty("user.dir") : outputPath;
+        if(Files.isDirectory(new File(outputPath).toPath())){
+            outputPath = outputPath +  File.separator + (outputFileName.isEmpty() ? DEFAULT_RESULT_FILENAME : outputFileName);
         }
+        log.info("outputPath: {}", outputPath);
         List<MetaTaskInfo> testSuites = ldtCore.diagnostic(inputPath);
         if(!testSuites.isEmpty()){
-            ldtCore.write(testSuites, outputPath, outputFileName);
+            ldtCore.write(testSuites, outputPath);
         }
     }
 }
